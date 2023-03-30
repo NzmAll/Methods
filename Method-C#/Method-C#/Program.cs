cb0b4b7ef87f9f5fb7e16ab7850c2c675e9c942d
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-03-30 15:47:01 +0400

    Some changes

--- Method-C#/Method-C#/Program.cs
+++ Method-C#/Method-C#/Program.cs
@@ -7,41 +7,42 @@ namespace Method_C_
 
         static void Main(string[] args)
         {
-            bool isTrue = true;
+            Console.Write("Adi daxil edin: ");
+            string name = Console.ReadLine()!;
+
             char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
             char[] lowerLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-            Console.Write("Stringi daxil edin: ");
-            string name = Console.ReadLine()!;
-
-            isTrue = Element(name, uppercaseLetters, lowerLetters);
-
-            if (isTrue == true)
-            {
-                Console.WriteLine("Herf yoxdur");
-            }
-            else if (isTrue == false)
-            {
-                Console.WriteLine("Herf var");
-            }
+            Element(name, uppercaseLetters, lowerLetters);
         }
 
-        static bool Element(string name, char[] uppercaseLetters, char[] lowerLetters)
+        static void Element(string name, char[] uppercaseLetters, char[] lowerLetters)
         {
-            bool isTrue = true;
+            bool check = false;
+
             for (int i = 0; i < name.Length; i++)
             {
                 for (int j = 0; j < uppercaseLetters.Length; j++)
                 {
                     if (name[i] == uppercaseLetters[j] || name[i] == lowerLetters[j])
                     {
-                        isTrue = false;
+                        check = true;
                         break;
                     }
+                    else
+                    {
+                        check = false;
+                    }
                 }
-                return isTrue;
             }
-            return isTrue;
+            if (check == true)
+            {
+                Console.WriteLine("Kodda herf var");
+            }
+            else
+            {
+                Console.WriteLine("Kodda reqem var");
+            }
         }
     }
 }
