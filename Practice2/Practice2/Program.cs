5e235dfbb4feaf132e710812fdbca90006fc4d70
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-03-30 16:32:09 +0400

    String within string

---
+++ Practice2/Practice2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Birinci arrayi daxil edin: ");
+            int[] numberOne = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            Console.Write("Ikinci arrayi daxil edin: ");
+            int[] numberTwo = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            Element(numberOne, numberTwo);
+        }
+
+        static void Element(int[] numberOne, int[] numberTwo)
+        {
+            bool check = false;
+
+            for (int i = 0; i < numberOne.Length; i++)
+            {
+                for (int j = 0; j < numberTwo.Length; j++)
+                {
+                    if (numberOne[i] == numberTwo[j])
+                    {
+                        check = true;
+                        break;
+                    }
+                }
+            }
+
+            if (check)
+            {
+                Console.WriteLine("Birincinin daxilinde ikinci arrayden ededler var");
+            }
+            else
+            {
+                Console.WriteLine("Birincinin daxilinde ikinci arrayden ededler yoxdur");
+            }
+        }
+    }
+}
